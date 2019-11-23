3e08a718b5667bf7422a22de43355d58e1d104a0
jdcarbeck <jdcarbeck@gmail.com>
2019-11-23 19:01:11 +0000

    latest working stoping at main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,16 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 module Main where
 
 import System.Random
-import Data.Char
 import Data.Matrix
+import qualified Data.Vector as V
+
+import Control.Applicative
+import Control.Monad
+import Data.IORef
+
+import Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
 
 import Game
 import Solver
@@ -11,26 +18,55 @@ import Solver
 type Move = ((Int,Int),Bool)
 
 main :: IO ()
-main = do
+main =  do
     seed <- newStdGen
-    let board = newGame 5 5 2 seed
-    --     (state,sol) = makeMove (1,1) True board
-    -- putStrLn $ printBoard (state,sol)
-    -- let adjPoints = getAdj (state,sol)
-    -- putStrLn $ show $ adjPoints
-    -- let possibleMovesU = findNeighbors (head adjPoints) state '*'
-    -- let possibleMovesF = findNeighbors (head adjPoints) state 'F'
-    -- putStrLn $ "# of Possible moves Unknown " ++ (show $ possibleMovesU)
-    -- putStrLn $ "# of Possible moves Flagged " ++ (show $ possibleMovesF)
-    -- putStrLn $ show $ digitToInt (state!(head adjPoints))
-    -- let possibleMoves = getPossibleMoves (head adjPoints) state
-    -- let madeMoves = makeMoves possibleMovesU True
-    -- putStrLn $ show $ possibleMoves
-    -- putStrLn $ show $ madeMoves
-    -- let (cord, mvType) = aiMove (state,sol)
-    -- let board'' = makeMove cord mvType (state,sol)
-    -- putStrLn $ show $ printBoard board''
-    minesweeper board
+    let board = newGame 10 10 1 seed
+    let board' = makeMove (1,1) True board
+    startGUI defaultConfig (setup board')
+
+
+setup :: Board -> Window -> UI()
+setup b window = void $ do
+    return window # set title "Sweeper"
+    aiMove <- UI.button #+ [ string "AI Move" ]
+    moveType <- UI.input
+                    # set UI.type_ "checkbox" 
+                    # set (attr "id") "flag"
+    moveLabel <- UI.label #+ [ string "Flag" ]
+                    # set (attr "for") "flag"
+    let board = mkBoard b
+    
+    getBody window #+ [ grid board, element aiMove, element moveType, element moveLabel ]
+
+
+mkBoard :: Board -> [[UI Element]]
+mkBoard (state, _) = elBoard state (nrows state) (ncols state)
+
+elBoard :: Field -> Int -> Int -> [[UI Element]]
+elBoard f 1 y = [(elRow f 1 y)]
+elBoard f x y = (elBoard f (x-1) y) ++ [(elRow f x y)]
+
+elRow :: Field -> Int -> Int -> [UI Element]
+elRow f x 1 = (elCell (x,1) $ f!(x,1)):[]
+elRow f x y = (elRow f x (y-1)) ++ [(elCell (x,y) $ f!(x,y))]
+
+elCell :: (Int,Int) -> Char -> UI Element
+elCell p c | c == '*' = UI.button #+ [ string (c:[]) ] 
+                            # set style [("border-radius","3px"), ("margin","2px"), ("border-style","solid"), ("height","30px"), ("width","30px"), ("outline", "0px")]
+                            #. (show p)
+            | c == 'F' = UI.button #+ [ string (c:[]) ] 
+                            # set style [("border-radius","3px"), ("margin","2px"), ("border-style","solid"), ("height","30px"), ("width","30px"), ("outline", "0px")]
+                            #. (show p)
+            | c == 'B' = UI.button #+ [ string (c:[]) ] 
+                            # set style [("border-radius","3px"), ("margin","2px"), ("border-style","solid"), ("height","30px"), ("width","30px"), ("outline", "0px")]
+                            #. (show p)  
+            | c == ' ' = UI.button #+ [ string ('0':[]) ] 
+                            # set style [("color", "#FFFFFF"),("border-radius","3px"), ("margin","2px"), ("border-style","solid"), ("height","30px"), ("width","30px"), ("outline", "0px")]
+                            #. (show p)
+            | otherwise = UI.button #+ [ string (c:[]) ] 
+                            # set style [("border-radius","3px"), ("margin","2px"), ("border-style","solid"), ("height","30px"), ("width","30px"), ("outline", "0px")]
+                            #. (show p)
+
 
 minesweeper :: Board -> IO ()
 minesweeper (state, sol) = do
